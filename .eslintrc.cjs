1aba0d87d1ae8f984664e49f30955a84006cb8a9
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2022-09-25 13:19:25 +0900

    chore(lint): add react-hooks lint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,6 +2,7 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
   ],
   parser: '@typescript-eslint/parser',
   plugins: [
