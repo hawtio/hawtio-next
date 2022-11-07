1c96e355d5f3ed38726073eb9545450d39c20f05
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2022-11-08 01:44:16 +0900

    fix: app rendering test

--- src/hawtio/Hawtio.test.tsx
+++ src/hawtio/Hawtio.test.tsx
@@ -1,13 +1,17 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
 import { HawtioContextProvider } from './context'
 import { Hawtio } from './Hawtio'
 
-test('renders page', () => {
-  render(
-    <HawtioContextProvider>
-      <Hawtio />
-    </HawtioContextProvider>
-  )
-  const example = screen.queryByText('Hawtio')
-  expect(example).toBeInTheDocument()
+describe('Hawtio', () => {
+  test('renders page', async () => {
+    render(
+      <HawtioContextProvider>
+        <Hawtio />
+      </HawtioContextProvider>
+    )
+    await waitFor(() => {
+      const example = screen.queryByText('Hawtio')
+      expect(example).toBeInTheDocument()
+    })
+  })
 })
