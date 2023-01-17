d810ded71af8eddb7a4f78a8c235a6b12322a520
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2023-01-17 22:48:21 +0900

    feat(preferences): add Logs preferences #5

---
+++ src/hawtio/core/logging.test.ts
@@ -0,0 +1,12 @@
+import { Logger } from './logging'
+
+describe('Logger', () => {
+  test('getAvailableChildLoggers', () => {
+    expect(Logger.getChildLoggers()).toEqual([])
+    expect(Logger.getAvailableChildLoggers()).toEqual([])
+    Logger.get('hawtio-core-test')
+    const availableLoggers = Logger.getAvailableChildLoggers()
+    expect(availableLoggers).toHaveLength(1)
+    expect(availableLoggers[0].name).toEqual('hawtio-core-test')
+  })
+})
--- src/hawtio/core/logging.ts
+++ src/hawtio/core/logging.ts
@@ -1,18 +1,24 @@
+import { stringSorter } from '@hawtio/util/strings'
 import jsLogger, { GlobalLogger, ILogger, ILogLevel } from 'js-logger'
+import { is, object, type } from 'superstruct'
 
 export const STORAGE_KEY_LOG_LEVEL = 'core.logging.logLevel'
 export const STORAGE_KEY_CHILD_LOGGERS = 'core.logging.childLoggers'
 
 interface HawtioLogger extends GlobalLogger {
-  loggers: { [name: string]: ILogger }
+  getChildLoggers(): ChildLogger[]
+  getAvailableChildLoggers(): ChildLogger[]
+  addChildLogger(logger: ChildLogger): void
+  updateChildLogger(name: string, level: ILogLevel | string): void
+  removeChildLogger(logger: ChildLogger): void
 }
 
-interface ChildLogger {
+export interface ChildLogger {
   name: string
   filterLevel: ILogLevel
 }
 
-class HawtioLoggerImpl implements HawtioLogger {
+class LocalStorageHawtioLogger implements HawtioLogger {
   TRACE = jsLogger.TRACE
   DEBUG = jsLogger.DEBUG
   INFO = jsLogger.INFO
@@ -29,13 +35,22 @@ class HawtioLoggerImpl implements HawtioLogger {
   error = jsLogger.error
   time = jsLogger.time
   timeEnd = jsLogger.timeEnd
-  setLevel = jsLogger.setLevel
   getLevel = jsLogger.getLevel
   enabledFor = jsLogger.enabledFor
 
   useDefaults = jsLogger.useDefaults
   setHandler = jsLogger.setHandler
   createDefaultHandler = jsLogger.createDefaultHandler
+
+  private readonly LOG_LEVEL_MAP: { [name: string]: ILogLevel } = {
+    'TRACE': this.TRACE,
+    'DEBUG': this.DEBUG,
+    'INFO': this.INFO,
+    'TIME': this.TIME,
+    'WARN': this.WARN,
+    'ERROR': this.ERROR,
+    'OFF': this.OFF,
+  } as const
 
   get(name: string): ILogger {
     if (this.loggers[name]) {
@@ -46,24 +61,44 @@ class HawtioLoggerImpl implements HawtioLogger {
     return logger
   }
 
-  loggers: { [name: string]: ILogger } = {}
+  setLevel(level: ILogLevel | string) {
+    const logLevel = this.toLogLevel(level)
+    jsLogger.setLevel(logLevel)
+    this.saveLogLevel(logLevel)
+  }
+
+  private loggers: { [name: string]: ILogger } = {}
 
   constructor() {
     try {
       const logLevel = this.loadLogLevel()
-      this.setLevel(logLevel)
+      jsLogger.setLevel(logLevel)
     } catch (e) {
-      console.error("Failed to load log level from local storage:", e)
+      console.error('Failed to load log level from local storage:', e)
     }
 
     try {
       const childLoggers = this.loadChildLoggers()
       childLoggers.forEach(logger => this.get(logger.name).setLevel(logger.filterLevel))
     } catch (e) {
-      console.error("Failed to load child loggers from local storage:", e)
+      console.error('Failed to load child loggers from local storage:', e)
     }
 
     this.setHandler(this.createDefaultHandler())
+  }
+
+  private toLogLevel(level: ILogLevel | string): ILogLevel {
+    let logLevel = this.INFO
+    if (typeof level === 'string') {
+      logLevel = this.LOG_LEVEL_MAP[level]
+      if (!logLevel) {
+        console.error('Unknown log level:', level)
+        return this.INFO
+      }
+    } else {
+      logLevel = level
+    }
+    return logLevel
   }
 
   private loadLogLevel(): ILogLevel {
@@ -71,10 +106,68 @@ class HawtioLoggerImpl implements HawtioLogger {
     return logLevel ? JSON.parse(logLevel) : this.INFO
   }
 
+  private saveLogLevel(level: ILogLevel) {
+    localStorage.setItem(STORAGE_KEY_LOG_LEVEL, JSON.stringify(level))
+  }
+
   private loadChildLoggers(): ChildLogger[] {
     const childLoggers = localStorage.getItem(STORAGE_KEY_CHILD_LOGGERS)
     return childLoggers ? JSON.parse(childLoggers) : []
   }
+
+  private saveChildLoggers(loggers: ChildLogger[]) {
+    localStorage.setItem(STORAGE_KEY_CHILD_LOGGERS, JSON.stringify(loggers))
+  }
+
+  getChildLoggers(): ChildLogger[] {
+    const childLoggers = this.loadChildLoggers()
+    childLoggers.sort((a, b) => stringSorter(a.name, b.name))
+    return childLoggers
+  }
+
+  getAvailableChildLoggers(): ChildLogger[] {
+    const allLoggers: ChildLogger[] = []
+    Object.values(this.loggers).forEach(logger => {
+      // reflectively access 'context' property of js-logger Logger object
+      if (is(logger, type({ context: object() }))) {
+        allLoggers.push(logger.context as unknown as ChildLogger)
+      } else {
+        console.error('Logger does not have context:', logger)
+      }
+    })
+    const childLoggers = this.getChildLoggers()
+    const availableLoggers = allLoggers.filter(logger => !childLoggers.some(l => l.name === logger.name))
+    availableLoggers.sort((a, b) => stringSorter(a.name, b.name))
+    return availableLoggers
+  }
+
+  addChildLogger(logger: ChildLogger): void {
+    const childLoggers = this.getChildLoggers()
+    childLoggers.push(logger)
+    this.saveChildLoggers(childLoggers)
+    this.get(logger.name).setLevel(logger.filterLevel)
+  }
+
+  updateChildLogger(name: string, level: ILogLevel | string): void {
+    const logLevel = this.toLogLevel(level)
+    const updated = this.getChildLoggers().map(logger => {
+      if (logger.name === name) {
+        logger.filterLevel = logLevel
+      }
+      return logger
+    })
+    this.saveChildLoggers(updated)
+    this.get(name).setLevel(logLevel)
+  }
+
+  removeChildLogger(logger: ChildLogger) {
+    const removed = this.getChildLoggers().filter(l => l.name !== logger.name)
+    this.saveChildLoggers(removed)
+    this.get(logger.name).setLevel(this.getLevel())
+  }
 }
 
-export const Logger = new HawtioLoggerImpl()
+/**
+ * Hawtio logger
+ */
+export const Logger = new LocalStorageHawtioLogger()
