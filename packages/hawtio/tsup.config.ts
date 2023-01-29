49d2d4950d3785835f6cb6c012be01a96e9c14d2
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2023-01-29 17:17:34 +0900

    fix: fix yarn test

--- packages/hawtio/tsup.config.ts
+++ packages/hawtio/tsup.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'tsup'
 
-// eslint-disable-next-line import/no-default-export
 export default defineConfig({
+  entry: ['src/index.ts'],
   dts: true,
   sourcemap: true,
-  clean: true,
   loader: {
     '.svg': 'dataurl',
     '.jpg': 'dataurl',
