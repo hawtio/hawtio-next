b4ed71c4c394affb523b08efcd00395ca3978b3c
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2023-02-01 15:56:21 +0900

    feat(connect): make Connect plugin aware of '/proxy/enabled' endpoint

--- packages/hawtio/src/plugins/connect/globals.ts
+++ packages/hawtio/src/plugins/connect/globals.ts
@@ -2,3 +2,4 @@ import { Logger } from '@hawtio/core'
 
 export const pluginName = 'hawtio-connect'
 export const log = Logger.get(pluginName)
+export const proxyEnabledPath = 'proxy/enabled'
--- packages/hawtio/src/plugins/connect/index.ts
+++ packages/hawtio/src/plugins/connect/index.ts
@@ -1,8 +1,10 @@
 import { hawtio } from '@hawtio/core'
 import { helpRegistry } from '@hawtio/help/registry'
 import { preferencesRegistry } from '@hawtio/preferences/registry'
+import $ from 'jquery'
 import { Connect } from './Connect'
 import { ConnectPreferences } from './ConnectPreferences'
+import { log, proxyEnabledPath } from './globals'
 import help from './help.md'
 import { jolokiaService } from './jolokia-service'
 
@@ -12,11 +14,35 @@ export const connect = () => {
     title: 'Connect',
     path: '/connect',
     component: Connect,
-    isActive: async () => {
-      const jolokiaUrl = await jolokiaService.getJolokiaUrl()
-      return jolokiaUrl === null
-    },
+    isActive: isActive,
   })
   helpRegistry.add('connect', 'Connect', help, 11)
   preferencesRegistry.add('connect', 'Connect', ConnectPreferences, 11)
+}
+
+async function isActive(): Promise<boolean> {
+  const proxyEnabled = await isProxyEnabled()
+  if (!proxyEnabled) {
+    return false
+  }
+
+  const jolokiaUrl = await jolokiaService.getJolokiaUrl()
+  return jolokiaUrl === null
+}
+
+async function isProxyEnabled(): Promise<boolean> {
+  return new Promise<boolean>((resolve) => {
+    $.ajax(proxyEnabledPath)
+      .done((data: string) => {
+        // Disable proxy only when explicitly disabled
+        const enabled = data.trim() !== 'false'
+        log.debug('Proxy enabled:', enabled)
+        resolve(enabled)
+      })
+      .fail((xhr: JQueryXHR) => {
+        // Silently ignore and enable it when the path is not available
+        log.debug('Failed to fetch', proxyEnabledPath, ':', xhr.status, xhr.statusText)
+        resolve(true)
+      })
+  })
 }
