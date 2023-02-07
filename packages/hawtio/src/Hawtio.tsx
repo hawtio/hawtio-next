72d02b3f84c152ff3c496a0532c8e7e5bddf81b9
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2023-02-07 13:32:22 +0900

    feat(router): auto-configure basename from html header base href #68

--- packages/hawtio/src/Hawtio.tsx
+++ packages/hawtio/src/Hawtio.tsx
@@ -3,16 +3,18 @@ import { BrowserRouter } from 'react-router-dom'
 import { HawtioContextProvider } from './context'
 import './Hawtio.css'
 import { HawtioPage } from './ui/page/HawtioPage'
+import { documentBase } from './util/documents'
 
 export type HawtioProps = {
-  basepath: string
+  basepath?: string
 }
 
 export const Hawtio: React.FunctionComponent<HawtioProps> = props => {
   const { basepath } = props
+  const basename = basepath ? basepath : documentBase()
   return (
     <HawtioContextProvider>
-      <BrowserRouter basename={basepath}>
+      <BrowserRouter basename={basename}>
         <HawtioPage />
       </BrowserRouter>
     </HawtioContextProvider>
