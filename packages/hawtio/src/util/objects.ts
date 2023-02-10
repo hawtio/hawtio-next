9c524368fdb1cad6975c3b5625c973e4ba537b1b
phantomjinx <p.g.richardson@phantomjinx.co.uk>
2023-02-10 23:34:18 +0000

    chore: Removal of clone library
    * Addition of small utility method and removal of clone
      dependency

--- packages/hawtio/src/util/objects.ts
+++ packages/hawtio/src/util/objects.ts
@@ -3,6 +3,10 @@ export function isObject(value: unknown): value is object {
   return value != null && (type === 'object' || type === 'function')
 }
 
+export function cloneObject<T>(value: T): T {
+  return JSON.parse(JSON.stringify(value))
+}
+
 export function isEmpty(obj: object): boolean {
   return Object.keys(obj).length === 0
 }
