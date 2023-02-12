93cfc4478a1b977dd8bbcd063ba3f5d7c1fc5519
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2023-02-12 16:12:19 +0900

    feat(about): About modal with component versions updated from hawtconfig.json #27

--- packages/hawtio/src/index.ts
+++ packages/hawtio/src/index.ts
@@ -1,3 +1,8 @@
+import { configManager } from './core'
+
 export * from './core'
 export * from './plugins'
 export * from './Hawtio'
+
+// Register Hawtio React component version
+configManager.addProductInfo('Hawtio React', '__PACKAGE_VERSION_PLACEHOLDER__')
