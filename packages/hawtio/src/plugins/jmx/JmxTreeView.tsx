78d5570abc22dc0226afb31960ef0cf27b21e345
JoshiRaez <jraez@redhat.com>
2023-02-27 11:56:24 +0100

    Prettier formatting

--- packages/hawtio/src/plugins/jmx/JmxTreeView.tsx
+++ packages/hawtio/src/plugins/jmx/JmxTreeView.tsx
@@ -6,45 +6,45 @@ import { MBeanTreeContext } from './context'
 import './JmxTreeView.css'
 
 export const JmxTreeView: React.FunctionComponent = () => {
-  const { tree, node, setNode } = useContext(MBeanTreeContext);
-  const [expanded, setExpanded] = useState(false);
-  const [filteredTree, updateTree] = useState(tree.getTree());
+  const { tree, node, setNode } = useContext(MBeanTreeContext)
+  const [expanded, setExpanded] = useState(false)
+  const [filteredTree, updateTree] = useState(tree.getTree())
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const onSearch = (event: ChangeEvent<HTMLInputElement>) => {
-    if(!event.target.value) updateTree(tree.getTree());
-    
-    const treeElements = lookupSearchInTree(event.target.value, tree.getTree());
+    if (!event.target.value) updateTree(tree.getTree())
+
+    const treeElements = lookupSearchInTree(event.target.value, tree.getTree())
 
     if (treeElements.length == 0) updateTree(tree.getTree())
-    else updateTree(treeElements);
+    else updateTree(treeElements)
   }
 
   const onSelect = (event: React.MouseEvent<Element, MouseEvent>, item: TreeViewDataItem) => {
     setNode(item as MBeanNode)
   }
 
-  const lookupSearchInTree = (search: string, tree?: MBeanNode[]) : MBeanNode[] => {
-    if (tree?.length == 0) return [];
-    
-    let results : MBeanNode[] = [];
+  const lookupSearchInTree = (search: string, tree?: MBeanNode[]): MBeanNode[] => {
+    if (tree?.length == 0) return []
+
+    let results: MBeanNode[] = []
 
     for (let parentNode of tree || []) {
-      if(parentNode.name.toLowerCase().includes(search.toLowerCase())) {
-        results = results.concat(parentNode);
+      if (parentNode.name.toLowerCase().includes(search.toLowerCase())) {
+        results = results.concat(parentNode)
       } else {
-        const resultsInSubtree = lookupSearchInTree(search, parentNode.children);
+        const resultsInSubtree = lookupSearchInTree(search, parentNode.children)
 
         if (resultsInSubtree.length != 0) {
-          const parentNodeCloned = Object.assign({}, parentNode); 
-          parentNodeCloned.children = resultsInSubtree;
+          const parentNodeCloned = Object.assign({}, parentNode)
+          parentNodeCloned.children = resultsInSubtree
 
-          results = results.concat(parentNodeCloned);
+          results = results.concat(parentNodeCloned)
         }
       }
     }
 
-    return results;
+    return results
   }
 
   return (
@@ -53,7 +53,7 @@ export const JmxTreeView: React.FunctionComponent = () => {
       data={filteredTree}
       hasGuides={true}
       hasSelectableNodes={true}
-      activeItems={node ? [node] : [] }
+      activeItems={node ? [node] : []}
       allExpanded={expanded}
       onSelect={onSelect}
       toolbar={<PluginTreeViewToolbar onSearch={onSearch} onSetExpanded={setExpanded} />}
