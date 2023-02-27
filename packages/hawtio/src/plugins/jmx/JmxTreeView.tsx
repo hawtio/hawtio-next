d3d1bee4dfca743e85e1d6e88c865d0042b2e6b2
JoshiRaez <jraez@redhat.com>
2023-02-27 10:22:48 +0100

    JMXSearch and JMX Highlight (24 and 72)

--- packages/hawtio/src/plugins/jmx/JmxTreeView.tsx
+++ packages/hawtio/src/plugins/jmx/JmxTreeView.tsx
@@ -6,23 +6,54 @@ import { MBeanTreeContext } from './context'
 import './JmxTreeView.css'
 
 export const JmxTreeView: React.FunctionComponent = () => {
-  const { tree, setNode } = useContext(MBeanTreeContext)
-  const [expanded, setExpanded] = useState(false)
+  const { tree, node, setNode } = useContext(MBeanTreeContext);
+  const [expanded, setExpanded] = useState(false);
+  const [filteredTree, updateTree] = useState(tree.getTree());
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const onSearch = (event: ChangeEvent<HTMLInputElement>) => {
-    // TODO
+    if(!event.target.value) updateTree(tree.getTree());
+    
+    const treeElements = lookupSearchInTree(event.target.value, tree.getTree());
+
+    if (treeElements.length == 0) updateTree(tree.getTree())
+    else updateTree(treeElements);
   }
 
   const onSelect = (event: React.MouseEvent<Element, MouseEvent>, item: TreeViewDataItem) => {
     setNode(item as MBeanNode)
   }
 
+  const lookupSearchInTree = (search: string, tree?: MBeanNode[]) : MBeanNode[] => {
+    if (tree?.length == 0) return [];
+    
+    let results : MBeanNode[] = [];
+
+    for (let parentNode of tree || []) {
+      if(parentNode.name.toLowerCase().includes(search.toLowerCase())) {
+        results = results.concat(parentNode);
+      } else {
+        const resultsInSubtree = lookupSearchInTree(search, parentNode.children);
+
+        if (resultsInSubtree.length != 0) {
+          const parentNodeCloned = Object.assign({}, parentNode); 
+          parentNodeCloned.children = resultsInSubtree;
+
+          results = results.concat(parentNodeCloned);
+        }
+      }
+    }
+
+    return results;
+  }
+
   return (
     <TreeView
       id='jmx-tree-view'
-      data={tree.getTree()}
+      data={filteredTree}
       hasGuides={true}
+      hasSelectableNodes={true}
+      activeItems={node ? [node] : [] }
       allExpanded={expanded}
       onSelect={onSelect}
       toolbar={<PluginTreeViewToolbar onSearch={onSearch} onSetExpanded={setExpanded} />}
