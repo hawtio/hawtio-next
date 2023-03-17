38459abf30fbc01b4f93fc537763a9f65f69aabb
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2023-03-17 12:48:54 +0900

    chore(help,preferences): export registries for external plugins #32

--- packages/hawtio/src/index.ts
+++ packages/hawtio/src/index.ts
@@ -1,8 +1,10 @@
 import { configManager } from './core'
 
 export * from './core'
-export * from './plugins'
 export * from './Hawtio'
+export { helpRegistry } from './help'
+export * from './plugins'
+export { preferencesRegistry } from './preferences'
 
 // Register Hawtio React component version
 configManager.addProductInfo('Hawtio React', '__PACKAGE_VERSION_PLACEHOLDER__')
--- packages/hawtio/src/preferences/index.ts
+++ packages/hawtio/src/preferences/index.ts
@@ -1,2 +1,3 @@
 export * from './HawtioPreferences'
 export * from './preferences-service'
+export * from './registry'
