0ee772f19297708a536a32406a482e8c8cf04484
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2023-03-18 17:17:47 +0900

    feat(connect): switch to file upload for import connections modal #67

--- packages/hawtio/src/plugins/connect/connections.ts
+++ packages/hawtio/src/plugins/connect/connections.ts
@@ -19,52 +19,67 @@ export interface Connection {
 export const ADD = 'ADD'
 export const UPDATE = 'UPDATE'
 export const DELETE = 'DELETE'
+export const IMPORT = 'IMPORT'
 export const RESET = 'RESET'
 
 export type ConnectionsAction =
   | { type: typeof ADD; connection: Connection }
   | { type: typeof UPDATE; name: string; connection: Connection }
   | { type: typeof DELETE; name: string }
+  | { type: typeof IMPORT; connections: Connection[] }
   | { type: typeof RESET }
+
+function addConnection(state: Connections, connection: Connection): Connections {
+  if (state[connection.name]) {
+    // TODO: error handling
+    return state
+  }
+  return { ...state, [connection.name]: connection }
+}
+
+function updateConnection(state: Connections, name: string, connection: Connection): Connections {
+  if (name === connection.name) {
+    // normal update
+    if (!state[connection.name]) {
+      // TODO: error handling
+      return state
+    }
+    return { ...state, [connection.name]: connection }
+  }
+
+  // name change
+  if (state[connection.name]) {
+    // TODO: error handling
+    return state
+  }
+  return Object.fromEntries(
+    Object.entries(state).map(([k, v]) => (k === name ? [connection.name, connection] : [k, v])),
+  )
+}
+
+function deleteConnection(state: Connections, name: string): Connections {
+  const newState = { ...state }
+  delete newState[name]
+  return newState
+}
 
 export function reducer(state: Connections, action: ConnectionsAction): Connections {
   switch (action.type) {
     case ADD: {
       const { connection } = action
-      if (state[connection.name]) {
-        // TODO: error handling
-        return state
-      } else {
-        return { ...state, [connection.name]: connection }
-      }
+      return addConnection(state, connection)
     }
     case UPDATE: {
       const { name, connection } = action
-      if (name === connection.name) {
-        // normal update
-        if (state[connection.name]) {
-          return { ...state, [connection.name]: connection }
-        } else {
-          // TODO: error handling
-          return state
-        }
-      } else {
-        // name change
-        if (state[connection.name]) {
-          // TODO: error handling
-          return state
-        } else {
-          return Object.fromEntries(
-            Object.entries(state).map(([k, v]) => (k === name ? [connection.name, connection] : [k, v])),
-          )
-        }
-      }
+      return updateConnection(state, name, connection)
     }
     case DELETE: {
       const { name } = action
-      const newState = { ...state }
-      delete newState[name]
-      return newState
+      return deleteConnection(state, name)
+    }
+    case IMPORT: {
+      const { connections } = action
+      return connections.reduce((newState, conn) => addConnection(newState, conn), state)
     }
     case RESET:
       return {}
