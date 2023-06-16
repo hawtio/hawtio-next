3fb03925fae48e25ed7f9758bc3a409972e43680
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2023-06-16 19:08:22 +0900

    refactor(shared): refactoring to workspace

--- packages/hawtio/src/plugins/shared/globals.ts
+++ packages/hawtio/src/plugins/shared/globals.ts
@@ -2,6 +2,3 @@ import { Logger } from '@hawtiosrc/core'
 
 export const pluginName = 'hawtio-shared'
 export const log = Logger.get(pluginName)
-
-export const HAWTIO_REGISTRY_MBEAN = 'hawtio:type=Registry'
-export const HAWTIO_TREE_WATCHER_MBEAN = 'hawtio:type=TreeWatcher'
