262d21adfc8a31b98a473e615b1c6a4cc9ca9ec7
JoshiRaez <jraez@redhat.com>
2023-06-22 15:11:49 +0200

    test(#258): Added tests for checking Ids are expected in both domain and mock tree

--- packages/hawtio/src/plugins/shared/tree/tree.test.ts
+++ packages/hawtio/src/plugins/shared/tree/tree.test.ts
@@ -1,3 +1,4 @@
+import { escapeHtmlId } from '@hawtiosrc/util/jolokia'
 import { workspace } from '../workspace'
 import { MBeanNode } from './node'
 import { treeProcessorRegistry } from './processor-registry'
@@ -60,16 +61,129 @@ describe('MBeanTree', () => {
     wkspTree.forEach(path, _ => (counter = counter + 1))
     expect(counter).toEqual(path.length)
   })
+
+  test('IDs should be concatenation of {parent}[-folder]-({element}[-folder]) on domain tree', async () => {
+    const allNodes: MBeanNode[] = []
+    const recursivelyGetAllNodesOnTree = (tree: MBeanNode[]) => {
+      tree.forEach((node: MBeanNode) => {
+        allNodes.push(node)
+        recursivelyGetAllNodesOnTree(node.getChildren())
+      })
+    }
+    const getExpectedIdRecursivelyFromParentNode = (node: MBeanNode): string => {
+      //Can't access node.idSeparator as is private. And in any case, we should modify this whenever it changes
+      const idSeparator = '-'
+      const folderDenomination = '-folder'
+      const currentNodeExpectedPartOfId =
+        escapeHtmlId(node.name) +
+        (node.getChildren().length !== 0 && !node.name.includes('XNIO-2') ? folderDenomination : '')
+
+      if (!node.parent) return currentNodeExpectedPartOfId
+      return getExpectedIdRecursivelyFromParentNode(node.parent) + idSeparator + currentNodeExpectedPartOfId
+    }
+    recursivelyGetAllNodesOnTree(wkspTree.getTree())
+
+    allNodes.forEach(node => {
+      expect(node.id).toEqual(getExpectedIdRecursivelyFromParentNode(node))
+    })
+  })
+
+  test('IDs should be concatenation of {parent}[-folder]-({element}[-folder]) on mock tree', () => {
+    //The object names are dummys because they are not used for the ids.
+    const treeNodes = [
+      createFolder('mbean1', [
+        createFolder('mbean1-1', [
+          createFolder('mbean1-1-1', [
+            createNode('mbean1-1-1-1', 'objectName1-1-1-1'),
+            createNode('mbean1-1-1-2', 'objectName1-1-1-2'),
+          ]),
+          createNode('mbean1-1-2', 'objectName1-1-2'),
+          createNode('mbean1-1-3', 'objectName1-1-3'),
+        ]),
+        createNode('mbean1-2', 'objectName1-2'),
+        createFolder('mbean1-3', [createNode('mbean1-3-1', 'objectName1-3-1')]),
+      ]),
+      createFolder('mbean2', [createNode('mbean2-1', 'objectName2-1'), createNode('mbean2-2', 'objectName2-2')]),
+      createNode('mbean3', 'objectName3'),
+    ]
+
+    const pathToExpectedIds = [
+      {
+        path: ['mbean1'],
+        expectedId: 'mbean1-folder',
+      },
+      {
+        path: ['mbean1', 'mbean1-1'],
+        expectedId: 'mbean1-folder-mbean1-1-folder',
+      },
+      {
+        path: ['mbean1', 'mbean1-1', 'mbean1-1-1'],
+        expectedId: 'mbean1-folder-mbean1-1-folder-mbean1-1-1-folder',
+      },
+      {
+        path: ['mbean1', 'mbean1-1', 'mbean1-1-1', 'mbean1-1-1-1'],
+        expectedId: 'mbean1-folder-mbean1-1-folder-mbean1-1-1-folder-mbean1-1-1-1',
+      },
+      {
+        path: ['mbean1', 'mbean1-1', 'mbean1-1-1', 'mbean1-1-1-2'],
+        expectedId: 'mbean1-folder-mbean1-1-folder-mbean1-1-1-folder-mbean1-1-1-2',
+      },
+      {
+        path: ['mbean1', 'mbean1-1', 'mbean1-1-2'],
+        expectedId: 'mbean1-folder-mbean1-1-folder-mbean1-1-2',
+      },
+      {
+        path: ['mbean1', 'mbean1-1', 'mbean1-1-3'],
+        expectedId: 'mbean1-folder-mbean1-1-folder-mbean1-1-3',
+      },
+      {
+        path: ['mbean1', 'mbean1-2'],
+        expectedId: 'mbean1-folder-mbean1-2',
+      },
+      {
+        path: ['mbean1', 'mbean1-3'],
+        expectedId: 'mbean1-folder-mbean1-3-folder',
+      },
+      {
+        path: ['mbean1', 'mbean1-3', 'mbean1-3-1'],
+        expectedId: 'mbean1-folder-mbean1-3-folder-mbean1-3-1',
+      },
+      {
+        path: ['mbean2'],
+        expectedId: 'mbean2-folder',
+      },
+      {
+        path: ['mbean2', 'mbean2-1'],
+        expectedId: 'mbean2-folder-mbean2-1',
+      },
+      {
+        path: ['mbean2', 'mbean2-2'],
+        expectedId: 'mbean2-folder-mbean2-2',
+      },
+      {
+        path: ['mbean3'],
+        expectedId: 'mbean3',
+      },
+    ]
+
+    const tree = MBeanTree.createFromNodes('thisIdIsNotUsedForMBeanIds', treeNodes)
+
+    pathToExpectedIds.forEach(({ path, expectedId }) => {
+      expect(tree.navigate(...path)?.id).toEqual(expectedId)
+    })
+  })
 })
 
 function createNode(name: string, objectName: string): MBeanNode {
   const node = new MBeanNode(null, name, false)
   node.objectName = objectName
+  node.initId(false)
   return node
 }
 
 function createFolder(name: string, children: MBeanNode[]): MBeanNode {
   const folder = new MBeanNode(null, name, true)
-  folder.children = children
+  children.forEach(child => folder.adopt(child))
+  folder.initId(true)
   return folder
 }
