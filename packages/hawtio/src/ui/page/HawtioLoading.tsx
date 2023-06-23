b1af6b1a10ab63078becf665f61a54fc72a835f9
mmelko <mmelko@redhat.com>
2023-06-23 15:40:43 +0200

    chore: Center the Spinner in the loading screen and add Loading message

--- packages/hawtio/src/ui/page/HawtioLoading.tsx
+++ packages/hawtio/src/ui/page/HawtioLoading.tsx
@@ -1,10 +1,18 @@
-import { Page, PageSection, Spinner } from '@patternfly/react-core'
+import { Bullseye, Page, Spinner, Text, TextContent, TextVariants } from '@patternfly/react-core'
 import React from 'react'
 
 export const HawtioLoading: React.FunctionComponent = () => (
   <Page>
-    <PageSection>
-      <Spinner isSVG aria-label='Loading Hawtio' />
-    </PageSection>
+    <Bullseye>
+      <div style={{ justifyContent: 'center' }}>
+        <Spinner diameter='60px' isSVG aria-label='Loading Hawtio' />
+
+        <TextContent>
+          <Text className={'--pf-global--Color--200'} component={TextVariants.h3}>
+            Loading ...
+          </Text>
+        </TextContent>
+      </div>
+    </Bullseye>
   </Page>
 )
