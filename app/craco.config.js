2e78ad31e3b3953d6af523c59ef86a33e3cc28fc
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2023-10-18 18:17:24 +0900

    chore(app): update @hawtio/react required version in craco

--- app/craco.config.js
+++ app/craco.config.js
@@ -32,7 +32,7 @@ module.exports = {
             '@hawtio/react': {
               singleton: true,
               // Hardcoding needed because it cannot handle yarn 'workspace:*' version
-              requiredVersion: '^0.5.0',
+              requiredVersion: '^0.6.0',
             },
           },
         }),
