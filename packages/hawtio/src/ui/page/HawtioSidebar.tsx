1c7608eb0dffefb15b993d0afc58e7974fc1f42c
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2023-11-27 13:47:58 +0900

    fix(ui): sidebar plugin navitem doesn't get active when selected

--- packages/hawtio/src/ui/page/HawtioSidebar.tsx
+++ packages/hawtio/src/ui/page/HawtioSidebar.tsx
@@ -7,11 +7,18 @@ export const HawtioSidebar: React.FunctionComponent = () => {
   const { plugins } = useContext(PageContext)
   const { pathname } = useLocation()
 
+  const pathMatch = (path: string, pluginPath: string) => {
+    if (!pluginPath.startsWith('/')) {
+      pluginPath = '/' + pluginPath
+    }
+    return path.startsWith(pluginPath)
+  }
+
   const pageNav = (
     <Nav theme='dark'>
       <NavList>
         {plugins.map(plugin => (
-          <NavItem key={plugin.id} isActive={plugin.path === pathname}>
+          <NavItem key={plugin.id} isActive={pathMatch(pathname, plugin.path)}>
             <NavLink to={plugin.path}>{plugin.title}</NavLink>
           </NavItem>
         ))}
