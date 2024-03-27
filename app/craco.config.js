9cdddafac7ba10249a0d2bd514ad136a299e8087
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2024-03-27 14:37:32 +0900

    fix(app): fix build after upgrading to keycloak.js 24.0.2

--- app/craco.config.js
+++ app/craco.config.js
@@ -60,6 +60,9 @@ module.exports = {
         fallback: {
           path: require.resolve('path-browserify'),
           os: require.resolve('os-browserify'),
+          crypto: require.resolve('crypto-browserify'),
+          stream: require.resolve('stream-browserify'),
+          vm: require.resolve('vm-browserify'),
         },
       }
 
