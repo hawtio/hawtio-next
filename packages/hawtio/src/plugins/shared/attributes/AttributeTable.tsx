06441357002eb46b90f0a2bc97f113e1d36b8b4d
mmelko <mmelko@redhat.com>
2024-04-08 16:15:21 +0200

    refactor(AttributeTable) Refactor AttributeTable to use TableComposable

--- packages/hawtio/src/plugins/shared/attributes/AttributeTable.tsx
+++ packages/hawtio/src/plugins/shared/attributes/AttributeTable.tsx
@@ -3,9 +3,9 @@ import { JmxContentMBeans } from '@hawtiosrc/plugins/shared/JmxContentMBeans'
 import { AttributeValues } from '@hawtiosrc/plugins/shared/jolokia-service'
 import { humanizeLabels } from '@hawtiosrc/util/strings'
 import { Card } from '@patternfly/react-core'
-import { Table, TableBody, TableHeader, TableProps } from '@patternfly/react-table'
+import { TableComposable, Tbody, Td, Th, Thead, Tr } from '@patternfly/react-table'
 import { Response } from 'jolokia.js'
-import { useContext, useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { HawtioEmptyCard } from '../HawtioEmptyCard'
 import { HawtioLoadingCard } from '../HawtioLoadingCard'
 import { MBeanNode } from '../tree'
@@ -17,7 +17,7 @@ export const AttributeTable: React.FunctionComponent = () => {
   const [attributesList, setAttributesList] = useState<{ [name: string]: AttributeValues }>({})
   const [isReading, setIsReading] = useState(false)
 
-  const attributesEntries = Object.values(attributesList)
+  const attributesEntries = Object.values(attributesList) ?? []
 
   function checkIfAllMBeansHaveSameAttributes(attributesEntries: AttributeValues[]): boolean {
     if (attributesEntries.length <= 1) {
@@ -28,6 +28,7 @@ export const AttributeTable: React.FunctionComponent = () => {
     if (!firstEntry) {
       return false
     }
+
     const firstAttrsLength = Object.keys(firstEntry).length
     if (attributesEntries.some(attrs => Object.keys(attrs).length !== firstAttrsLength)) {
       return false
@@ -51,9 +52,7 @@ export const AttributeTable: React.FunctionComponent = () => {
 
       for (const node of currentSelection.getChildren()) {
         if (!node || !node?.objectName) continue
-
-        const attrs = await attributeService.read(node.objectName)
-        childrenMbeansAttributes[node.objectName] = attrs
+        childrenMbeansAttributes[node.objectName] = await attributeService.read(node.objectName)
       }
 
       setAttributesList({ ...childrenMbeansAttributes })
@@ -105,17 +104,27 @@ export const AttributeTable: React.FunctionComponent = () => {
   }
 
   const labels = Object.keys(attributesEntries[0] ?? {})
-  const columns: TableProps['cells'] = labels.map(label => humanizeLabels(label))
-  const rows: TableProps['rows'] = attributesEntries.map(attribute =>
-    labels.map(label => JSON.stringify(attribute[label])),
-  )
 
   return (
     <Card isFullHeight>
-      <Table aria-label='MBeans' variant='compact' cells={columns} rows={rows}>
-        <TableHeader className={'attribute-table'} />
-        <TableBody />
-      </Table>
+      <TableComposable aria-label='MBeans' variant='compact'>
+        <Thead>
+          <Tr>
+            {labels.map(label => (
+              <Th key={'header-' + label}>{humanizeLabels(label)}</Th>
+            ))}
+          </Tr>
+        </Thead>
+        <Tbody>
+          {attributesEntries.map((attribute, index) => (
+            <Tr key={'attribute-' + index}>
+              {labels.map((label, index) => (
+                <Td key={'data-' + label + index}>{JSON.stringify(attribute[label])}</Td>
+              ))}
+            </Tr>
+          ))}
+        </Tbody>
+      </TableComposable>
     </Card>
   )
 }
