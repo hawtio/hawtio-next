fcbdf315ba58340d2ae0609c6c7c67b6c015cfde
mmelko <mmelko@redhat.com>
2024-06-12 14:16:48 +0200

    fix(CamelIcon): remove defaultProps

--- packages/hawtio/src/plugins/camel/icons/CamelImageIcon.tsx
+++ packages/hawtio/src/plugins/camel/icons/CamelImageIcon.tsx
@@ -2,16 +2,11 @@ type CamelImageIconProps = {
   name: string
   svg: string
   size: number
-} & typeof defaultImageIconProps
-
-const defaultImageIconProps = {
-  size: 16,
 }
 
 const CamelImageIcon = (props: CamelImageIconProps) => {
-  return <img src={props.svg} width={props.size + 'px'} height={props.size + 'px'} alt={props.name} />
+  const { name, svg, size = 16 } = props
+  return <img src={svg} width={size + 'px'} height={size + 'px'} alt={name} />
 }
-
-CamelImageIcon.defaultProps = defaultImageIconProps
 
 export { CamelImageIcon }
