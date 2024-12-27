27f0a8d137389b1856ded74a2b9eb2228b72bd91
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2024-12-27 15:14:52 +0900

    fix(console-status): fix plugin name

--- packages/hawtio/src/plugins/console-status/globals.ts
+++ packages/hawtio/src/plugins/console-status/globals.ts
@@ -1,6 +1,6 @@
 import { Logger } from '@hawtiosrc/core/logging'
 
 export const pluginId = 'consolestatus'
-export const pluginName = 'Console Status'
+export const pluginName = 'hawtio-consolestatus'
 export const pluginPath = '/consolestatus'
 export const log = Logger.get(pluginName)
