bc80333e759f83ed17b62ecd2ab1a1401c008089
Grzegorz Grzybek <gr.grzybek@gmail.com>
2025-07-08 12:04:16 +0200

    feat(ui): Add verbose flag to <HawtioInitialization>

--- packages/hawtio/src/ui/init/HawtioInitialization.tsx
+++ packages/hawtio/src/ui/init/HawtioInitialization.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 
 import { configManager, type InitializationTasks, TaskState } from '@hawtiosrc/core/config-manager'
 
-export const HawtioInitialization: React.FC = () => {
+export const HawtioInitialization: React.FC<{ verbose: boolean }> = ({ verbose = false }) => {
   const [tasks, setTasks] = useState<InitializationTasks>(configManager.getInitializationTasks())
 
   useEffect(() => {
@@ -33,60 +33,91 @@ export const HawtioInitialization: React.FC = () => {
 
   const items: { item: string; ready: TaskState }[] = []
   const plugins: { item: string; ready: TaskState }[] = []
+  const dots: TaskState[] = []
   let finish = false
 
   for (const item in tasks) {
     if (tasks[item]?.group === 'plugins') {
       plugins.push({ item: item, ready: tasks[item] ? tasks[item]!.ready : TaskState.started })
+      dots.push(tasks[item] ? tasks[item]!.ready : TaskState.started)
     } else if (tasks[item]?.group === 'finish') {
       finish = true
     } else {
       items.push({ item: item, ready: tasks[item] ? tasks[item]!.ready : TaskState.started })
+      dots.push(tasks[item] ? tasks[item]!.ready : TaskState.started)
     }
   }
 
-  function icon(state: TaskState): string {
-    switch (state) {
-      case TaskState.skipped:
-        return '➖'
-      case TaskState.finished:
-        return '✅'
-      case TaskState.error:
-        return '❌'
-      case TaskState.started:
-      default:
-        return '❔'
-    }
-  }
-
-  return (
-    <>
-      <div className='hwt-loading'>
-        <h4 className={finish ? 'ready' : 'not-ready'}>Hawtio initialization ...</h4>
-        {items.length > 0 ? (
-          <ul>
-            {items.map((el: { item: string; ready: TaskState }, idx: number) => (
-              <li key={idx} className={el.ready == TaskState.started ? 'not-ready' : 'ready'}>
-                <span>{icon(el.ready)}</span>
-                {el.item}
-              </li>
+  if (!verbose) {
+    return (
+      <>
+        <div className='hwt-loading'>
+          <h4>
+            {dots.map((el: TaskState) => (
+                <span className={tickClass(el)}>#</span>
             ))}
-          </ul>
-        ) : null}
-        {plugins.length > 0 ? (
-          <>
-            <h4 className={finish ? 'ready' : 'not-ready'}>Hawtio plugins ...</h4>
+          </h4>
+        </div>
+      </>
+    )
+  } else {
+    return (
+      <>
+        <div className='hwt-loading'>
+          <h4 className={finish ? 'ready' : 'not-ready'}>Hawtio initialization ...</h4>
+          {items.length > 0 ? (
             <ul>
-              {plugins.map((el: { item: string; ready: TaskState }, idx: number) => (
+              {items.map((el: { item: string; ready: TaskState }, idx: number) => (
                 <li key={idx} className={el.ready == TaskState.started ? 'not-ready' : 'ready'}>
                   <span>{icon(el.ready)}</span>
                   {el.item}
                 </li>
               ))}
             </ul>
-          </>
-        ) : null}
-      </div>
-    </>
-  )
+          ) : null}
+          {plugins.length > 0 ? (
+            <>
+              <h4 className={finish ? 'ready' : 'not-ready'}>Hawtio plugins ...</h4>
+              <ul>
+                {plugins.map((el: { item: string; ready: TaskState }, idx: number) => (
+                  <li key={idx} className={el.ready == TaskState.started ? 'not-ready' : 'ready'}>
+                    <span>{icon(el.ready)}</span>
+                    {el.item}
+                  </li>
+                ))}
+              </ul>
+            </>
+          ) : null}
+        </div>
+      </>
+    )
+  }
+}
+
+function icon(state: TaskState): string {
+  switch (state) {
+    case TaskState.skipped:
+      return '➖'
+    case TaskState.finished:
+      return '✅'
+    case TaskState.error:
+      return '❌'
+    case TaskState.started:
+    default:
+      return '❔'
+  }
+}
+
+function tickClass(state: TaskState): string {
+  switch (state) {
+    case TaskState.skipped:
+      return 'tick tick-skipped'
+    case TaskState.finished:
+      return 'tick tick-finished'
+    case TaskState.error:
+      return 'tick tick-error'
+    case TaskState.started:
+    default:
+      return 'tick tick-started'
+  }
 }
