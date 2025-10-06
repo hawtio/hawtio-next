172769d536e6685028081db57f4d74452cb6ba95
Grzegorz Grzybek <gr.grzybek@gmail.com>
2025-10-06 10:58:04 +0200

    chore(proxy): Emulate Hawtio's handling of Basic authentication challenge

--- packages/backend-middleware/src/proxy.ts
+++ packages/backend-middleware/src/proxy.ts
@@ -27,13 +27,23 @@ export async function proxy(uri: string, req: Request, res: Response) {
   } catch (error) {
     if (isAxiosError(error) && error.response) {
       const res2 = error.response
+      const newHeaders = Object.assign({}, res2.headers as { [key: string]: string})
+      if (res2.status == 401 && newHeaders['www-authenticate']) {
+        // emulate Hawtio's probing of remote Jolokia. Without "WWW-Authenticate: Basic ...",
+        // browser never displays native credentials dialog, so we can handle it nicer
+        let v = newHeaders['www-authenticate'] as string
+        if (v.toLowerCase().startsWith("basic")) {
+          v = 'Hawtio original-scheme="Basic" ' + v.substring(6, v.length)
+          newHeaders['WWW-Authenticate'] = v
+        }
+      }
       switch (res2.status) {
         case 401:
         case 403:
         case 429:
           log.info('Authentication failed on remote server:', error.status, error.message, uri)
-          log.debug('Response headers:', res2.headers)
-          res.header(res2.headers).sendStatus(res2.status)
+          log.debug('Response headers:', newHeaders)
+          res.header(newHeaders).sendStatus(res2.status)
           break
         default:
           handleError(String(error))
