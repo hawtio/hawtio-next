c4b5d48dfad094ac9ffc149262f114fc58ce9520
Grzegorz Grzybek <gr.grzybek@gmail.com>
2025-10-17 18:15:07 +0200

    test: app/webpack.config.js enables OIDC by default
    When we start the application in `app` using `yarn webpack server`, the
    backend starts with user named `developer` logged in. If we logout,
    Hawtio will present auth method selection screen as configured by
    /auth/config/login endpoint.
    
    OIDC method will be available. In order to make it
    really work, hawtio/hawtio/examples/keycloak-integration/run-keycloak.sh
    has to be started.

--- app/webpack.config.cjs
+++ app/webpack.config.cjs
@@ -291,7 +291,7 @@ module.exports = (_, args) => {
 
           /* OpenID Connect */
 
-          const oidcEnabled = false
+          const oidcEnabled = true
           //const entraIDOidcConfig = {
           //  method: 'oidc',
           //  provider: 'https://login.microsoftonline.com/11111111-2222-3333-4444-555555555555/v2.0',
@@ -330,28 +330,15 @@ module.exports = (_, args) => {
           /* Available authentication methods to configure <HawtioLogin> page */
 
           const authLoginConfig = [
-            {
-              method: 'basic',
-              name: 'Basic Authentication',
-              realm: 'Hawtio Realm',
-            },
+            // {
+            //   method: 'basic',
+            //   name: 'Basic Authentication',
+            //   realm: 'Hawtio Realm',
+            // },
             {
               // special method that is declared at server side, but never handled in @hawtio/react
               method: 'external',
               name: 'Spring Security',
-            },
-            {
-              // Actual configuration of OIDC provider will be added from
-              // /auth/config/oidc endpoint
-              // TODO: support more OIDC methods - like Keycloak, Azure and Spring Authorization Server at the same time
-              method: 'oidc',
-              name: 'OpenID Connect (Keycloak)',
-            },
-            {
-              // Actual configuration of Keycloak provider will be added from
-              // /keycloak/client-config endpoint
-              method: 'keycloak',
-              name: 'Keycloak (keycloak.js)',
             },
             // {
             //   "method": "form",
@@ -372,9 +359,31 @@ module.exports = (_, args) => {
               passwordField: 'password',
             },
           ]
+
+          if (keycloakEnabled) {
+            authLoginConfig.push({
+              // Actual configuration of Keycloak provider will be added from
+              // /keycloak/client-config endpoint
+              method: 'keycloak',
+              name: 'Keycloak (keycloak.js)',
+            })
+          }
+          if (oidcEnabled) {
+            authLoginConfig.push({
+              // Actual configuration of OIDC provider will be added from
+              // /auth/config/oidc endpoint
+              // TODO: support more OIDC methods - like Keycloak, Azure and Spring Authorization Server at the same time
+              method: 'oidc',
+              name: 'OpenID Connect (Keycloak)',
+            })
+          }
+
           devServer.app.get(`${publicPath}/auth/config/login`, (_, res) => {
-            res.json([])
-            // res.json(authLoginConfig)
+            if (!(keycloakEnabled || oidcEnabled)) {
+              res.json([])
+            } else {
+              res.json(authLoginConfig)
+            }
           })
 
           // Hawtio backend middleware should be run before other middlewares (thus 'unshift')
