959948df941e9706e0542d098b07c00b42d532e7
JoshiRaez <joshiraez@li-316c72cc-3382-11b2-a85c-bf38cd610183.ibm.com>
2025-10-21 15:29:51 +0200

    Reverting Grezgorz suggesiton for now because it'd only work for hawtio-standalone launched from a Java application. To be revisited later

--- packages/hawtio/src/plugins/diagnostics/index.ts
+++ packages/hawtio/src/plugins/diagnostics/index.ts
@@ -3,7 +3,7 @@ import { helpRegistry } from '@hawtiosrc/help/registry'
 import { pluginId, pluginPath } from './globals'
 import help from './help.md'
 import { flightRecorderService } from './flight-recorder-service'
-import { connectService } from '../shared'
+import { jolokiaService } from '../shared'
 
 const order = 19
 
@@ -17,8 +17,12 @@ export const diagnostics: HawtioPlugin = () => {
         order,
         component: m.Diagnostics,
         isActive: async () => {
+          const jolokiaUrl = await jolokiaService.getFullJolokiaUrl()
+
           return (
-            (await flightRecorderService.hasFlightRecorderMBean()) && connectService.getCurrentConnectionId() === null
+            Boolean(await flightRecorderService.hasFlightRecorderMBean()) &&
+            ['localhost', '127.0.0.1', '::1', '192.168.', '10.0'].filter(localUrl => jolokiaUrl.includes(localUrl))
+              .length != 0
           )
         },
       }
